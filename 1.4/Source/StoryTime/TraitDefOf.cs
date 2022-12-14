c2c34bcaff374b94569a728edd08c49621cabd93
Isengriff <115048453+Lindonrow@users.noreply.github.com>
2022-12-14 01:50:54 -0700

    Chapter 12: Time of the Traits
    -Moved files around
    -Added new traits
    -Trait colors

---
+++ 1.4/Source/StoryTime/TraitDefOf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace StoryTime
+{
+    [DefOf]
+    public static class TraitDefOf
+    {
+        public static TraitDef Buglover;
+        public static TraitDef Kind;
+        public static TraitDef RareGenius;
+        static TraitDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(ThingDefOf));
+    }
+}
