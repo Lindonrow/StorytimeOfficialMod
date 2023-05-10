1f5d09b175b4e355b7b124c185ad937bc8c1b815
Isengriff <115048453+Lindonrow@users.noreply.github.com>
2023-05-10 00:43:56 -0600

    Great Proletarian Cultural Revolution Chapter 17: Igniting the Furnace of My Impotent Rage!
    -Resounding melodies for the Gibbonous Moon event now echo the People's triumph!
    -Programming bugs plaguing Lore Loss have been eradicated by the strength of Mao Zedong Thought!
    -The rising dawn of a new placeholder logo heralds the imminent arrival of the official banner!
    -In the face of broken bean man types, our hearts burn with revolutionary fury! We will find a solution and crush all who stand in our way!
    -Returnedthe bean men to their natural state of nakedness, striking a blow against capitalist-imperialist fashion!

---
+++ 1.4/Source/StoryTime/MainMenuPatch.cs
@@ -0,0 +1,35 @@
+﻿
+using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+
+namespace StoryTime
+{
+    [StaticConstructorOnStartup]
+    [HarmonyPatch]
+    public static class MainMenuPatch
+    {
+        private static readonly Texture2D storytimemenu = ContentFinder<Texture2D>.Get("UI/Menu/StorytimeMenu");
+
+        private static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return (MethodBase)AccessTools.Method(typeof(MainMenuDrawer), "MainMenuOnGUI");
+        }
+
+        private static IEnumerable<CodeInstruction> Transpiler(
+          IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (CodeInstruction codeInstruction in new List<CodeInstruction>(instructions))
+            {
+                if (codeInstruction.opcode == OpCodes.Ldsfld && (FieldInfo)codeInstruction.operand == AccessTools.Field(typeof(MainMenuDrawer), "TexTitle"))
+                    codeInstruction.operand = (object)AccessTools.Field(typeof(MainMenuPatch), "storytimemenu");
+                yield return codeInstruction;
+            }
+        }
+    }
+}
+
---
+++ 1.4/Source/StoryTime/MenuPatchOther.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace StoryTime
+{
+    [StaticConstructorOnStartup]
+    public class MenuPatch
+    {
+        static MenuPatch() => new Harmony("com.jonah.rimworld.mod.storytime").PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
