8a40965ca2e03147d49a40abfcbcac6271e8158f
Isengriff <115048453+Lindonrow@users.noreply.github.com>
2023-05-12 17:58:28 -0600

    Great Proletarian Cultural Revolution Chapter 19: The Glorious Transformation of Bean Man, Part 2!
    -Liberating the visual realm, proper graphics now adorn all bean man genes, magnifying their revolutionary essence!
    -Eradicating the chains of age-induced debuffs, bean men now toil with undeterred workspeed, a testament to their everlasting vigor!
    -Revolutionizing the genetic landscape, alterations have been made to bean man genes, suppressing unwanted traits and reinforcing proletarian purity!
    -Ushering a new era of bean mastery, the Bean Affinity stat governs bean-based crafting recipes, empowering the proletariat with culinary excellence!
    -Reestablishing the majestic Bean Man Racial Trait, magnifying Bean Affinity to unprecedented heights, unleashing the full might of bean-based ingenuity!
    -Unleashing the fury of the revolutionary struggle, new bean man PawnKinds emerge, infusing raids and groups with unparalleled intrigue!
    -Expanding the horizons of bean man genetics, fresh bean genes make their debut, laying the foundation for future bean man types in the grand revolution!

---
+++ 1.4/Source/StoryTime/BeanManAgeFix.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ATReforged
+{
+    internal class StatPart_Age_Patch
+    {
+        [HarmonyPatch(typeof(StatPart_Age), "ActiveFor")]
+        public class ActiveFor_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(Pawn pawn, ref bool __result)
+            {
+                if (!__result || !(pawn.ageTracker.CurLifeStage.defName == "BeanManAdultStage"))
+                    return;
+                __result = false;
+            }
+        }
+    }
+}
